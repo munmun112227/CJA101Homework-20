1f78cead1c848e446771ff630b75b633eae344f9
Jason Huang <virage1257@hotmail.com>
2025-03-18 21:35:18 +0800

    HomeWork4_2 is done

--- src/hw4/HomeWork4_2.java
+++ src/hw4/HomeWork4_2.java
@@ -2,5 +2,10 @@ package hw4;
 //請建立一個字串,經過程式執行後,輸入結果是反過來的
 //例如String s = “Hello World”,執行結果即為dlroW olleH
 public class HomeWork4_2 {
-
+	public static void main (String[] args) {
+		String s = "Hello World";
+		for(int i = s.length() -1 ; i > -1; i--) {
+			System.out.print(s.charAt(i));
+		}
+	}
 }
