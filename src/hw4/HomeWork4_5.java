952328eda21d74077280da06fdf5db464d55ba38
Jason Huang <virage1257@hotmail.com>
2025-03-20 22:44:26 +0800

    HomeWork4_5 is done

--- src/hw4/HomeWork4_5.java
+++ src/hw4/HomeWork4_5.java
@@ -7,16 +7,43 @@ import java.util.Scanner;
 //例:輸入 1984 9 8 三個號碼後,程式會顯示「輸入的日期為該年第252天」
 public class HomeWork4_5 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("請輸入日期，以空格分隔年月日");
-		int year = sc.nextInt();
-		int month = sc.nextInt();
-		int day = sc.nextInt();
 //		用陣列放非閏年和閏年的每月天數
 		int[][] monthDays = {
 				{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
 				{31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 		};
+		Scanner sc = new Scanner(System.in);
+		int year;
+		int month;
+		int day;
+		System.out.println("請輸入日期，以空格分隔年月日");
+		while(true){
+			year = sc.nextInt();
+			month = sc.nextInt();
+			day = sc.nextInt();
+			if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+				if(day <= monthDays[1][month-1]) {
+					break;
+				}
+				else {
+					System.out.println("異常，再輸入一次");
+					System.out.println("請輸入日期，以空格分隔年月日");
+					continue;
+				}
+			}
+			else {
+				if(day <= monthDays[0][month-1]) {
+					break;
+				}
+				else {
+					System.out.println("異常，再輸入一次");
+					System.out.println("請輸入日期，以空格分隔年月日");
+					continue;
+				}
+			}
+		}
+
+
 		
 		int days = 0;
 		for(int i = 0; i < (month - 1); i++) {
