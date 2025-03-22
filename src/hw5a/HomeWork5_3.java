97a42d25de7a1397fd2cf0a8f878647d61169c67
Jason Huang <virage1257@hotmail.com>
2025-03-22 21:52:34 +0800

    HomeWork5_3 is done

--- src/hw5a/HomeWork5_3.java
+++ src/hw5a/HomeWork5_3.java
@@ -1,8 +1,43 @@
 package hw5a;
 
+import java.util.Arrays;
+
 //利用Overloading,設計兩個方法int maxElement(int x[][])與double maxElement(double x[][]),
 //可以找出二維陣列的最大值並回傳
 
 public class HomeWork5_3 {
 
+	public static void main(String[] args) {
+		HomeWork5_3 h = new HomeWork5_3();
+		int[][] intArray = { { 1, 6, 3 }, { 9, 5, 2 } };
+		double[][] doubleArray = { { 1.2, 3.5, 2.2 }, { 7.4, 2.1, 8.2 } };
+		
+		System.out.println(h.maxElement(intArray));
+		System.out.println(h.maxElement(doubleArray));
+		
+	}
+
+	public int maxElement(int x[][]) {
+		int[] maxNum = new int[x.length];
+
+		for (int i = 0; i < x.length; i++) {
+			Arrays.sort(x[i]);
+			maxNum[i] = x[i][x[i].length - 1];
+		}
+
+		Arrays.sort(maxNum);
+		return maxNum[maxNum.length - 1];
+
+	}
+
+	public double maxElement(double x[][]) {
+		double[] maxNum = new double[x.length];
+		for (int i = 0; i < x.length; i++) {
+			Arrays.sort(x[i]);
+			maxNum[i] = x[i][x[i].length - 1];
+		}
+
+		Arrays.sort(maxNum);
+		return maxNum[maxNum.length - 1];
+	}
 }
