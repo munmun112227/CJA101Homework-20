c5a28c8e54191237ec7c0d5026b205076c861d3c
Jason Huang <virage1257@hotmail.com>
2025-03-22 18:27:35 +0800

    HomeWork5_1 is done

--- src/hw5a/HomeWork5_1.java
+++ src/hw5a/HomeWork5_1.java
@@ -1,7 +1,40 @@
 package hw5a;
 
+import java.util.Scanner; 
+
 //請設計一個方法為starSquare(int width, int height),當使用者鍵盤輸入寬與高時,即會印出對應的*長方形
 
 public class HomeWork5_1 {
 
+	public static void main(String[] args) {
+		HomeWork5_1 square = new HomeWork5_1();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("輸入兩個正整數，以空格區分");
+		int w, h;
+		while(true) {
+			w = sc.nextInt();
+			h = sc.nextInt();
+			if(w < -1 || h < -1) {
+				System.out.println("異常，重新輸入");
+				System.out.println("輸入兩個正整數，以空格區分");
+				continue;
+			}
+			else {
+				break;
+			}
+		}
+		
+		square.starSquare(w, h);
+		
+	}
+	
+	public void starSquare(int width, int height) {
+		
+		for(int i = 0; i < height; i++) {
+			for(int j = 0; j < width; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
 }
