2d8dd754192fb470057a5cbc7bb5fb8201064872
Jason Huang <virage1257@hotmail.com>
2025-04-01 18:32:29 +0800

    HomeWork7_5 is done

--- src/hw7/HomeWork7_5.java
+++ src/hw7/HomeWork7_5.java
@@ -1,7 +1,30 @@
 package hw7;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 //承上題,請寫一個程式,能讀取Object.ser這四個物件,並執行speak()方法觀察結果如何 (請利用多
 //型簡化本題的程式設計)
 public class HomeWork7_5 {
 
+	public static void main(String[] args) {
+		try {
+			FileInputStream fis = new FileInputStream("C:\\CJA101_Workspace\\Homework-20\\data\\Object.ser");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			
+
+			((Cat)ois.readObject()).speak();
+			((Dog)ois.readObject()).speak();
+			
+			ois.close();
+			fis.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
---
+++ src/hw7/Pet.java
@@ -0,0 +1,6 @@
+package hw7;
+
+public interface Pet {
+
+	public void speak();
+}
