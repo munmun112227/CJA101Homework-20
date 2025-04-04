4a1b0d003c2a87dfb8705955b7e53f0d13add3c7
Jason Huang <virage1257@hotmail.com>
2025-04-04 13:43:57 +0800

    first commit

---
+++ src/hw8/HomeWork8_1.java
@@ -0,0 +1,12 @@
+package hw8;
+
+//請建立一個Collection物件並將以下資料加入:
+//Integer(100)、Double(3.14)、Long(21L)、Short(“100”)、Double(5.1)、“Kitty”、Integer(100)、
+//Object物件、“Snoopy”、BigInteger(“1000”)
+//
+//印出這個物件裡的所有元素(使用Iterator, 傳統for與foreach)
+//移除不是java.lang.Number相關的物件
+//再次印出這個集合物件的所有元素,觀察是否已將非Number相關的物件移除成功
+public class HomeWork8_1 {
+
+}
