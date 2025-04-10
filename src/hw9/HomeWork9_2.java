30b94e191daaca02f40624d54811df611612111b
Jason Huang <virage1257@hotmail.com>
2025-04-10 09:00:21 +0800

    HomeWork9_2 is done

--- src/hw9/HomeWork9_2.java
+++ src/hw9/HomeWork9_2.java
@@ -7,6 +7,89 @@ package hw9;
 
 public class HomeWork9_2 {
 
+	public static void main(String[] args) {
+		Account acc = new Account();
+		TransactionsAdd ta = new TransactionsAdd(2000, "媽媽", acc);
+		TransactionsSub tb = new TransactionsSub(1000, "熊大", acc);
+		
+		ta.start();
+		tb.start();
+		
+	}
+}
+
+class Account {
+	private int balance = 0;
+
+	synchronized public void deposit(int amount, String name) {
+		while(balance < 3000) {
+			balance += amount;
+			System.out.println(name + "存了" + amount + "元，帳戶共有：" + balance + "元");
+			notify();
+ 		}
+		try {
+			System.out.println(name + "看到餘額在3000元以上，暫停存款");
+			System.out.println(name + "告知帳戶有錢了!");
+			wait(10000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	synchronized public void payment(int amount, String name) {
+		while(balance > 2000){
+			balance -= amount;
+			System.out.println(name + "領了" + amount + "元，帳戶共有：" + balance + "元");
+		}
+		try {
+			System.out.println(name + "看到帳戶沒錢，暫停提款");
+			System.out.println(name + "告知要匯錢了!");
+			notify();
+			wait(10000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
+class TransactionsAdd extends Thread {
+
+	Account acc;
+	int amount;
+	String name;
 	
+	public TransactionsAdd (int amount, String name, Account acc) {
+		this.amount = amount;
+		this.name = name;
+		this.acc = acc;
+	}
 	
+	public void run() {
+		for(int i = 1; i <= 10; i++) {
+			acc.deposit(amount, name);
+		}
+	}
+		
+}
+
+class TransactionsSub extends Thread {
+
+	Account acc;
+	int amount;
+	String name; 
+	
+	public TransactionsSub(int amount, String name, Account acc) {
+		this.amount = amount;
+		this.name = name;
+		this.acc = acc;
+	}
+	
+	public void run() {
+		for(int i = 1; i <= 10; i++) {
+			acc.payment(amount, name);
+		}
+	}
 }
